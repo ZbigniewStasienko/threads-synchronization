b62414e6b42946b0a1330f2a80b4160f2c955054
Zbyszek Stasienko <zbyszekstasienko@gmail.com>
2024-04-13 16:58:44 +0200

    working with thread library

--- project.cpp
+++ project.cpp
@@ -1,5 +1,4 @@
 #include <GLFW/glfw3.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <vector>
 #include <memory>
@@ -7,6 +6,8 @@
 #include <time.h>
 #include <chrono>
 #include <stdio.h>
+#include <thread>
+#include <mutex>
 
 using namespace std;
 
@@ -15,9 +16,9 @@ struct ThreadData {
     float x;
     float y;
     float direction;
-    bool active;
-    bool finished;
-    pthread_t threadId;
+    volatile bool active;
+    volatile bool finished;
+    thread workerThread;
     float color[3];
 
     ThreadData(float s)
@@ -28,8 +29,6 @@ struct ThreadData {
     }
 };
 
-
-
 float colorSquare[3][3] = {
     {1.0f, 0.0f, 0.0f},
     {0.0f, 1.0f, 0.0f},
@@ -37,100 +36,90 @@ float colorSquare[3][3] = {
 };
 
 int currentColorIndex = 0;
-pthread_mutex_t colorMutex;
+mutex colorMutex;
 bool running = true;
 
 vector<shared_ptr<ThreadData>> threadsData;
-pthread_mutex_t threadMutex;
+mutex threadMutex;
 
-void* threadFunction(void* arg) {
-    auto data = *static_cast<shared_ptr<ThreadData>*>(arg);
+void threadFunction(shared_ptr<ThreadData> data) {
     bool reachedCenter = false;
     bool reachedStand = false;
 
     while (data->active) {
-        pthread_mutex_lock(&threadMutex);
-        data->x += data->speed;
-        data->y += data->direction;
-        if (!reachedCenter) {
-            if (data->x >= 0.0f) {
-                reachedCenter = true;
-                pthread_mutex_lock(&colorMutex);
-                int targetColorIndex = currentColorIndex;
-                pthread_mutex_unlock(&colorMutex);
-                if (targetColorIndex == 0) {
+        {
+            lock_guard<mutex> lk(threadMutex);
+            data->x += data->speed;
+            data->y += data->direction;
+        }
+
+        if (!reachedCenter && data->x >= 0.0f) {
+            reachedCenter = true;
+            lock_guard<mutex> colorLock(colorMutex);
+            if (currentColorIndex == 0) {
                     data->direction = (data->speed)/2;
-                } else if (targetColorIndex == 1) {
+                } else if (currentColorIndex == 1) {
                     data->direction = -(data->speed)/2;
-                }
             }
         }
-        if (!reachedStand) {
-            if (data->x >= 0.95f) {
-                reachedStand = true;
-                data->direction = 0.0f;
-                pthread_mutex_unlock(&threadMutex);
-                usleep(1000000);
-                pthread_mutex_lock(&threadMutex);
-            }
+
+        if (!reachedStand && data->x >= 0.95f) {
+            reachedStand = true;
+            data->direction = 0.0f;
+            usleep(1000000);
         }
+
         if (data->x >= 1.0f) {
             data->active = false;
         }
-        pthread_mutex_unlock(&threadMutex);
-        for (int i = 0; i < 10; ++i) {
-            if (!data->active) break;
-            usleep(5000);
-        }
+
+        usleep(50000);
     }
-    pthread_mutex_lock(&threadMutex);
-    data->finished = true;
-    pthread_mutex_unlock(&threadMutex);
-    return NULL;
+    {
+        lock_guard<mutex> lk(threadMutex);
+        data->finished = true;
+    }
 }
 
-void* colorChangeFunction(void* arg) {
+void colorChangeFunction() {
     int changeColorTime = 2;
     while (running) {
-        pthread_mutex_lock(&colorMutex);
-        currentColorIndex = (currentColorIndex + 1) % 3;
-        pthread_mutex_unlock(&colorMutex);
+        {
+            lock_guard<mutex> lk(colorMutex);
+            currentColorIndex = (currentColorIndex + 1) % 3;
+        }
         sleep(changeColorTime);
     }
-    return NULL;
 }
 
 void draw() {
     glClear(GL_COLOR_BUFFER_BIT);
     glLineWidth(2.0f);
 
-    pthread_mutex_lock(&threadMutex);
-    for (const auto& data : threadsData) {
-        if (data->active) {
-            glColor3fv(data->color);
-            glBegin(GL_LINES);
-            glVertex2f(data->x, data->y - 0.05f);
-            glVertex2f(data->x, data->y + 0.05f);
-            glEnd();
+    {
+        lock_guard<mutex> lk(threadMutex);
+        for (const auto& data : threadsData) {
+            if (data->active) {
+                glColor3fv(data->color);
+                glBegin(GL_LINES);
+                glVertex2f(data->x, data->y - 0.05f);
+                glVertex2f(data->x, data->y + 0.05f);
+                glEnd();
+            }
         }
     }
-    pthread_mutex_unlock(&threadMutex);
 
-    pthread_mutex_lock(&colorMutex);
     glColor3f(colorSquare[currentColorIndex][0], colorSquare[currentColorIndex][1], colorSquare[currentColorIndex][2]);
-    pthread_mutex_unlock(&colorMutex);
-
-    glLineWidth(4.0f);
     glBegin(GL_LINES);
     glVertex2f(0.0f, -0.1f);
     glVertex2f(0.0f, 0.1f);
     glEnd();
 
     float halfSide = 0.035f;
-
-    glColor3f(1.0f, 0.0f, 0.0f);
     float x = 1.0f;
     float y = 0.475f;
+
+    glColor3f(1.0f, 0.0f, 0.0f);
     glBegin(GL_QUADS);
     glVertex2f(x - halfSide, y + halfSide);
     glVertex2f(x + halfSide, y + halfSide);
@@ -177,58 +166,37 @@ int main() {
     glfwMakeContextCurrent(window);
     glOrtho(-1.0, 1.2, -1.0, 1.0, -1.0, 1.0);
 
-    pthread_mutex_init(&threadMutex, NULL);
-    pthread_mutex_init(&colorMutex, NULL);
-
-    pthread_t colorChangeThread;
-    pthread_create(&colorChangeThread, NULL, colorChangeFunction, NULL);
+    thread colorChangeThread(colorChangeFunction);
 
     auto lastCreationTime = chrono::steady_clock::now();
 
     while (!glfwWindowShouldClose(window)) {
         auto currentTime = chrono::steady_clock::now();
         float elapsed = chrono::duration<float>(currentTime - lastCreationTime).count();
-        float newThreadIncidence = 1.5f;
-        if (elapsed >= newThreadIncidence) {
-            float speed = (float)(rand() % 4 + 1) / 150.0f;
+        if (elapsed >= 1.5f) {
+            float speed = static_cast<float>(rand() % 4 + 1) / 150.0f;
             auto data = make_shared<ThreadData>(speed);
-            pthread_create(&data->threadId, NULL, threadFunction, &data);
+            data->workerThread = thread(threadFunction, data);
             threadsData.push_back(data);
             lastCreationTime = currentTime;
         }
         draw();
         glfwSwapBuffers(window);
         glfwPollEvents();
-
-        pthread_mutex_lock(&threadMutex);
-        auto it = remove_if(threadsData.begin(), threadsData.end(), [](const shared_ptr<ThreadData>& thread) {
-            return thread->finished;
-        });
-        threadsData.erase(it, threadsData.end());
-        pthread_mutex_unlock(&threadMutex);
     }
 
     running = false;
-    pthread_join(colorChangeThread, NULL);
+    colorChangeThread.join();
 
-    pthread_mutex_lock(&threadMutex);
     for (auto& thread : threadsData) {
         thread->active = false;
-    }
-    pthread_mutex_unlock(&threadMutex);
-
-    for (auto& thread : threadsData) {
-        pthread_join(thread->threadId, NULL);
+        if (thread->workerThread.joinable()) {
+            thread->workerThread.join();
+        }
     }
     
     glfwDestroyWindow(window);
     glfwTerminate();
 
-    pthread_mutex_destroy(&threadMutex);
-    pthread_mutex_destroy(&colorMutex);
-
-    threadsData.clear();
-
     return 0;
 }
-
